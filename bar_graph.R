5586d184f7079952f4e9937d5be5c8f71bb281f0
andreasbueckle <abueckle@indiana.edu>
2022-02-02 16:05:39 -0500

    Update bar_graph.R

--- bar_graph.R
+++ bar_graph.R
@@ -19,16 +19,25 @@ data <- renamed %>%
   mutate(
     remaining_scaled = remaining / 1000,
     days_remaining = yday(mdy(renamed$end))
+  ) %>%
+  filter(
+    remaining_scaled != 0
   )
 data
 
+ggplot(mtcars, aes(x = mpg, y = disp)) +
+  geom_point(aes(colour = factor(cyl))) +
+  scale_colour_brewer(palette = "BuPu")
 
-ggplot(data, aes(x = project, y = remaining_scaled, fill = days_remaining)) +
-  scale_fill_distiller(palette = "RdPu") +
-  geom_bar(stat = "identity") +
+ggplot(data, aes(x = project, y = days_remaining, fill = remaining_scaled)) +
+  # scale_fill_distiller(palette = "RdPu", direction=-1) +
+  scale_fill_brewer(palette = "Dark2") +
+  # scale_fill_brewer(direction=-1)+
+  geom_bar(stat = "identity", width = data$remaining_scaled / 1000) +
+  coord_flip() +
   theme_minimal() +
-  ylab("Remaining funds in 1000 USD") +
+  ylab("Remaining days to project end") +
   xlab("Project") +
-  labs(fill = "Remaining days of funding") +
+  labs(fill = "Remaining funds") +
   ggtitle("Remaining CNS Funds") +
-  geom_text(aes(label = remaining_scaled), nudge_y = 15, size = 3)
+  geom_text(aes(label = paste(remaining_scaled, "$")), nudge_y = 15, size = 3)
