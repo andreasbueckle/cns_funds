6801a2929901549238948f8e36c25ae25c22ec60
andreasbueckle <abueckle@indiana.edu>
2022-02-02 14:26:30 -0500

    Update bar_graph.R

--- bar_graph.R
+++ bar_graph.R
@@ -1,31 +1,34 @@
 library(tidyverse)
+library(lubridate)
 
 # Get this file from Andi as needed
-raw = read_csv("HBG-Funding - Sheet1.csv")
+raw <- read_csv("HBG-Funding - Feb2, 2022.csv")
 
-renamed = raw %>% 
+renamed <- raw %>%
   rename(
-  total = `total$`,
-  cns = `cns$`,
-  remaining = `remaing$on`
-) %>% 
-  mutate(
-    remaining = total - cns
+    total = `total$`,
+    cns = `cns$`,
+    remaining = `remaing$on`
   )
 renamed
 
-data = renamed %>% 
-  filter(!is.na(remaining) & remaining != 0) %>% 
+data <- renamed %>%
+  select(
+    -c("...8", "...9")
+  ) %>%
   mutate(
-    remaining_scaled = remaining/1000
+    remaining_scaled = remaining / 1000,
+    days_remaining = yday(mdy(renamed$end))
   )
 data
 
-  ggplot(data, aes(x = project, y = remaining_scaled, fill = project)) +
+
+ggplot(data, aes(x = project, y = remaining_scaled, fill = days_remaining)) +
+  scale_fill_distiller(palette = "RdPu") +
   geom_bar(stat = "identity") +
   theme_minimal() +
-  ylab("Remaining funds in 1000 USD")+
-  xlab("Project")+
-  ylim(0, max(data$remaining_scaled))+
-  labs(fill = "Project")+
-  ggtitle("Remaining CNS Funds")
+  ylab("Remaining funds in 1000 USD") +
+  xlab("Project") +
+  labs(fill = "Remaining days of funding") +
+  ggtitle("Remaining CNS Funds") +
+  geom_text(aes(label = remaining_scaled), nudge_y = 15, size = 3)
